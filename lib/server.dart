7981e37c8ac246c8b8f7d61674665d65eb3cd1fa
dave bound <dave.bound@synacor.com>
2020-08-15 13:18:14 +0100

    test

--- lib/server.dart
+++ lib/server.dart
@@ -1,24 +1,25 @@
 import 'package:flutter/material.dart';
 
-
-
 // make own custom stateless widget
 // everything is a widget and every widget is a class
-class HomeServer extends StatelessWidget {  // cant change over time      THIS SUPPORTS HOT RELOAD. DRIER CODE Dont Repeat Yourself DRY
-
+class HomeServer extends StatelessWidget {
+  // cant change over time      THIS SUPPORTS HOT RELOAD. DRIER CODE Dont Repeat Yourself DRY
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(      // container of my layout widgets
+    return Scaffold(
+      // container of my layout widgets
 
-      appBar:AppBar(
-        title:Text('Car Bluetooth Server'),     // params can be either props or widgets
+      appBar: AppBar(
+        title: Text(
+            'Car Bluetooth Server'), // params can be either props or widgets
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
 
+      body: Center(
 
-      body:Center(
+          /*
           child:FlatButton(
               onPressed: () {
                 print('console msg');
@@ -31,20 +32,45 @@ class HomeServer extends StatelessWidget {  // cant change over time      THIS S
               hoverColor:Colors.blue
             //elevation: 30,
           )
-      ),
+          */
 
 
+          child: Row(children: [
+        FlatButton(
+            onPressed: () {
+              print('clicked left');
+            },
+            child: Text(
+              'left',
+              style: TextStyle(color: Colors.white60),
+            ),
+            color: Colors.red,
+            hoverColor: Colors.blue
+            //elevation: 30,
+            ),
+        SizedBox(width: 20),
+        Text('0'),
+        SizedBox(width: 20),
+        FlatButton(
+            onPressed: () {
+              print('clicked right');
+            },
+            child: Text(
+              'right',
+              style: TextStyle(color: Colors.white60),
+            ),
+            color: Colors.red,
+            hoverColor: Colors.blue
+            //elevation: 30,
+            )
+      ])),
 
       floatingActionButton: FloatingActionButton(
         tooltip: 'Press Me',
-        child:Icon(Icons.insert_emoticon),
+        child: Icon(Icons.insert_emoticon),
         onPressed: () {},
         backgroundColor: Colors.green,
       ),
-
     );
-
-
-
   } // Build
 }
